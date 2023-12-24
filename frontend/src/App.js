39ec343c12b7fd6fbab9eb1bb72e4a1832234c54
Niladri Das <niladri.das@lpu.in>
2023-12-24 07:29:17 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+// Example services/authService.js
+const baseUrl = 'http://localhost:3000/api/auth';
+
+export const login = async (credentials) => {
+   // Implementation for login API call
+};
+
+export const register = async (userData) => {
+   // Implementation for user registration API call
+};
