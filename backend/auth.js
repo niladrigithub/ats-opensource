37fe6c93e7f41b9b92cb56daebc11d9f194054cd
Niladri Das <niladri.das@lpu.in>
2023-12-24 07:22:21 +0530

    Create auth.js

---
+++ backend/auth.js
@@ -0,0 +1,9 @@
+// Example auth.js
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+
+module.exports = router;
