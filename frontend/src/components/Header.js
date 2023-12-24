8c376d3d273904a4995b0d88cb97912552a6eb34
Niladri Das <niladri.das@lpu.in>
2023-12-24 07:27:02 +0530

    Create Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+// Example components/Header.js
+import React from 'react';
+
+const Header = () => {
+   return (
+      <header>
+         <h1>ATS-OpenSource</h1>
+         {/* Add navigation or other header elements */}
+      </header>
+   );
+};
+
+export default Header;
