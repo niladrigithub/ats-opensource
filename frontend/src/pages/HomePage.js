17980899fddfc4e6df422c68dd782f1f1c090d19
Niladri Das <niladri.das@lpu.in>
2023-12-24 07:28:12 +0530

    Create HomePage.js

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// Example pages/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+   return (
+      <div>
+         <h2>Welcome to ATS-OpenSource!</h2>
+         {/* Add content for the home page */}
+      </div>
+   );
+};
+
+export default HomePage;
