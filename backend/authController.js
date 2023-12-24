59ea81807bf3b20334aa92c55ff0ca059c2b5bb4
Niladri Das <niladri.das@lpu.in>
2023-12-24 07:22:42 +0530

    Create authController.js

---
+++ backend/authController.js
@@ -0,0 +1,10 @@
+// Example authController.js
+const User = require('../models/User');
+
+exports.login = async (req, res) => {
+   // Implementation for login
+};
+
+exports.register = async (req, res) => {
+   // Implementation for user registration
+};
